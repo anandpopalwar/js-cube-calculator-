a2140e7cb6323ab5ab95b42af88b6e9b50f7af69
anandpopalwar <anandpopalwar444@gmail.com>
2022-09-16 18:54:07 +0530

    1st commit

---
+++ app.js
@@ -0,0 +1,42 @@
+// function reset(){
+//     let number= document.getElementById('number').value
+//     number=''
+// }
+function square(){
+    let number= document.getElementById('number').value
+    document.getElementById('name').innerHTML=number*number
+    
+}
+function cube(){
+    let number= document.getElementById('number').value
+    document.getElementById('name').innerHTML=number*number*number
+}
+function x4(){
+    let number= document.getElementById('number').value
+    document.getElementById('name').innerHTML=number*number*number*number
+}
+function x5(){
+    let number= document.getElementById('number').value
+    document.getElementById('name').innerHTML=number*number*number*number*number
+}
+function x6(){
+    let number= document.getElementById('number').value
+    document.getElementById('name').innerHTML=number*number*number*number*number*number
+}
+function x7(){
+    let number= document.getElementById('number').value
+    document.getElementById('name').innerHTML=number*number*number*number*number*number*number
+}
+function x8(){
+    let number= document.getElementById('number').value
+    document.getElementById('name').innerHTML=number*number*number*number*number*number*number*number
+}
+function x9(){
+    let number= document.getElementById('number').value
+    document.getElementById('name').innerHTML=number*number*number*number*number*number*number*number*number
+}
+function x10(){
+    let number= document.getElementById('number').value
+    document.getElementById('name').innerHTML=number*number*number*number*number*number*number*number*number*number
+}
+
